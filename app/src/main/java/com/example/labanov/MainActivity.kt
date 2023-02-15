6900b051e4b3fb58b013bca19b9fd1d637a74cee
BuildTools <unconfigured@null.spigotmc.org>
2023-02-15 21:27:16 +0500

    v.1.1

--- app/src/main/java/com/example/labanov/MainActivity.kt
+++ app/src/main/java/com/example/labanov/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.labanov
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvBtn: TextView = findViewById(R.id.tvBtn) // knopka
+        tvBtn.setOnClickListener {
+            val intent = Intent(this@MainActivity, Splashmya::class.java)
+            startActivity(intent)
+            this@MainActivity.finish()
+        }
     }
 }
---
+++ app/src/main/java/com/example/labanov/Splashmya.kt
@@ -0,0 +1,16 @@
+package com.example.labanov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.content.Intent as Intent1
+
+class Splashmya : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashmya)
+
+
+        }
+    }
+}
